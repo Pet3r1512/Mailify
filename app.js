d4b47aa7ef5955e5c890765123e71cbc1e15b7c0
Thanh Phong <pttp15122002@gmail.com>
2023-03-14 11:13:09 +0700

    Create project

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+  , http = require('http')
+  , path = require('path')
+  , bodyParser = require('body-parser')
+  , favicon = require('serve-favicon')
+  , logger = require('morgan')
+  , methodOverride = require('method-override');
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(favicon(__dirname + '/public/images/favicon.png'));
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+if (app.get('env') == 'development') {
+  app.locals.pretty = true;
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});

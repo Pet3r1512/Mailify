7c0d1aaecec523aa03045a2e9cc1df46e84b0098
Thanh Phong <pttp15122002@gmail.com>
2023-03-19 11:34:04 +0700

    custome default colors

--- ViteJS/tailwind.config.cjs
+++ ViteJS/tailwind.config.cjs
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  plugins: [require('daisyui')],
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -7,5 +8,21 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#f43f5e",
+          "secondary": "#FFCCC9",
+          "accent": "#1FB2A5",
+          "neutral": "#191D24",
+          "base-100": "#2A303C",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
 }

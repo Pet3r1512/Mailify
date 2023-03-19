215254532a3306a8273705a739d8976875e4c3de
Thanh Phong <pttp15122002@gmail.com>
2023-03-19 11:43:43 +0700

    add icon

--- ViteJS/src/pages/home.jsx
+++ ViteJS/src/pages/home.jsx
@@ -31,7 +31,23 @@ function Hero() {
             You will love working with Mailify.
           </p>
           <a href="/register">
-            <button className="btn btn-outline btn-primary">Get Started</button>
+            <button className="btn btn-outline btn-primary flex justify-center items-center gap-1">
+              Get Started
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-5 h-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+                />
+              </svg>
+            </button>
           </a>
         </div>
         <img className="mr-0" srcset="/images/hero.png 3x" alt="" />

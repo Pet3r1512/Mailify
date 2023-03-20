7433613e8a6c63a71a6b256e0f656b22af2c0d9e
Thanh Phong <pttp15122002@gmail.com>
2023-03-20 07:21:01 +0700

    change prisma query data

--- ExpressJS/database/query.js
+++ ExpressJS/database/query.js
@@ -2,12 +2,14 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
 async function addUser(user) {
-    const { phonenumber, username } = user
+    const { fullname, phonenumber, username, password } = user
 
     await prisma.user.create({
         data: {
+            fullname: fullname,
             username: username,
             phonenumber: phonenumber,
+            password: password,
         }
     })
 }

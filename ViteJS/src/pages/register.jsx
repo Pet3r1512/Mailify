6531223f30a12ca60f5ac6f063eccb46cac55585
Thanh Phong <pttp15122002@gmail.com>
2023-03-21 21:34:19 +0700

    Improve UI

--- ViteJS/src/pages/register.jsx
+++ ViteJS/src/pages/register.jsx
@@ -4,7 +4,7 @@ import { HeadBar } from "../components/index";
 
 export default function RegisterPage() {
   return (
-    <div className="bg-white flex flex-col md:h-screen justify-center items-center">
+    <div className="bg-white flex flex-col min-h-screen justify-center items-center">
       <h1 className="text-4xl text-primary font-bold mb-16">
         Create New Account
       </h1>

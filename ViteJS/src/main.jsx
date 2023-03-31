b5223db5444e6edf65fb3bf2854a6d9ad80881d1
Thanh Phong <pttp15122002@gmail.com>
2023-03-31 20:59:36 +0700

    use customized MUI theme

--- ViteJS/src/main.jsx
+++ ViteJS/src/main.jsx
@@ -1,36 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { RouterProvider } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { RegisterPage, SignInPage, Setting } from "./pages";
-import TestLogIn from "./pages/testLogin";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/setting",
-    element: <Setting />,
-  },
-  {
-    path: "/register",
-    element: <RegisterPage />,
-  },
-  {
-    path: "/signin",
-    element: <SignInPage />,
-  },
-  {
-    path: "/testLogin",
-    element: <TestLogIn />,
-  },
-]);
+import theme from "./assets/theme";
+import router from "./routes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>
 );

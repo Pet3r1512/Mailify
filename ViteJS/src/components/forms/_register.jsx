fb2e5c80f41d29722aa011cc53c6400d696a2b66
Thanh Phong <pttp15122002@gmail.com>
2023-04-03 21:59:15 +0700

    onSubmit effect

--- ViteJS/src/components/forms/_register.jsx
+++ ViteJS/src/components/forms/_register.jsx
@@ -7,13 +7,15 @@ import {
   IconButton,
   InputLabel,
   CircularProgress,
+  Alert,
 } from "@mui/material";
 import { VisibilityOff, Visibility } from "@mui/icons-material";
 import { useNavigate } from "react-router-dom";
 
 export default function Register() {
   const [showPassword, setShowPassword] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isSubmit, setIsSubmit] = useState("");
+
   const {
     register,
     handleSubmit,
@@ -24,8 +26,8 @@ export default function Register() {
   const navigate = useNavigate();
 
   const onSubmit = async (data) => {
-    setIsLoading(true);
     await sleep(200);
+    setIsSubmit("on");
     await fetch("http://localhost:8080/api", {
       method: "POST",
       headers: {
@@ -38,90 +40,111 @@ export default function Register() {
       })
       .then((res) => {
         if (res.success === true) {
-          setIsLoading(false);
+          setIsSubmit("success");
+          sleep(400);
           return navigate("/");
         }
       });
   };
 
-  if (isLoading) return <CircularProgress />;
-
   return (
-    <form
-      className="flex flex-col gap-y-4 w-[600px] mx-auto"
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      {/* Full Name */}
-      <FormControl variant="outlined">
-        <InputLabel htmlFor="outlined-adornment-password" color="inputColor">
-          Full Name
-        </InputLabel>
-        <OutlinedInput
-          id="outlined-adornment-password"
-          fullWidth
-          color="inputColor"
-          label="Full Name"
-          {...register("fullname")}
-        />
-      </FormControl>
-      {/* Phone Number */}
-      <FormControl variant="outlined">
-        <InputLabel htmlFor="outlined-adornment-password" color="inputColor">
-          Phone Number
-        </InputLabel>
-        <OutlinedInput
-          id="outlined-adornment-password"
-          fullWidth
-          color="inputColor"
-          type="tel"
-          pattern="0[0-9]{9}"
-          label="Phone Number"
-          {...register("phonenumber")}
-        />
-      </FormControl>
-      {/* Username - Email Address */}
-      <FormControl variant="outlined">
-        <InputLabel htmlFor="outlined-adornment-password" color="inputColor">
-          Username
-        </InputLabel>
-        <OutlinedInput
-          id="outlined-adornment-password"
-          fullWidth
-          color="inputColor"
-          endAdornment={
-            <InputAdornment position="start">@mailify.com</InputAdornment>
-          }
-          label="Username"
-          {...register("username")}
-        />
-      </FormControl>
-      {/* Password */}
-      <FormControl variant="outlined" color="inputColor">
-        <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-        <OutlinedInput
-          id="outlined-adornment-password"
-          type={showPassword ? "text" : "password"}
-          fullWidth
-          endAdornment={
-            <InputAdornment position="end">
-              <IconButton
-                aria-label="toggle password visibility"
-                onClick={() => {
-                  setShowPassword(!showPassword);
-                }}
-                edge="end"
-              >
-                {showPassword ? <VisibilityOff /> : <Visibility />}
-              </IconButton>
-            </InputAdornment>
-          }
-          label="Password"
-          {...register("password")}
-        />
-      </FormControl>
-      <button className="btn btn-active btn-primary text-white">
-        <input type="submit" />
-      </button>
-    </form>
+    <>
+      <form
+        className="flex flex-col gap-y-4 w-[600px] mx-auto"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        {/* Full Name */}
+        <FormControl variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password" color="inputColor">
+            Full Name
+          </InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            fullWidth
+            required
+            color="inputColor"
+            label="Full Name"
+            {...register("fullname", {
+              pattern: /[A-Za-z]{3}/,
+            })}
+          />
+        </FormControl>
+        {/* Phone Number */}
+        <FormControl variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password" color="inputColor">
+            Phone Number
+          </InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            fullWidth
+            required
+            color="inputColor"
+            type="tel"
+            pattern="0[0-9]{9}"
+            label="Phone Number"
+            {...register("phonenumber")}
+          />
+        </FormControl>
+        {/* Username - Email Address */}
+        <FormControl variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password" color="inputColor">
+            Username
+          </InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            fullWidth
+            required
+            color="inputColor"
+            endAdornment={
+              <InputAdornment position="start">@mailify.com</InputAdornment>
+            }
+            label="Username"
+            {...register("username")}
+          />
+        </FormControl>
+        {/* Password */}
+        <FormControl variant="outlined" color="inputColor">
+          <InputLabel htmlFor="outlined-adornment-password">
+            Password
+          </InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={showPassword ? "text" : "password"}
+            fullWidth
+            required
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={() => {
+                    setShowPassword(!showPassword);
+                  }}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+            {...register("password")}
+          />
+        </FormControl>
+        <button className="btn btn-active btn-primary text-white">
+          <input type="submit" />
+        </button>
+      </form>
+      {isSubmit && (
+        <Alert
+          style={{
+            position: "absolute",
+            bottom: "5px",
+            left: "5px",
+          }}
+          severity="success"
+        >
+          Create new account successfully!!!
+        </Alert>
+      )}
+    </>
   );
 }

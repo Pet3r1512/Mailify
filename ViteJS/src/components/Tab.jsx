8c2b1464b3c10fbb9d74e758b036b4635ab25fa4
Thanh Phong <pttp15122002@gmail.com>
2023-04-04 16:22:53 +0700

    change favicon icon

--- ViteJS/src/components/Tab.jsx
+++ ViteJS/src/components/Tab.jsx
@@ -5,6 +5,7 @@ export default function Tab({ name }) {
   return (
     <Helmet>
       <meta charSet="utf-8" />
+      <link rel="icon" href="favicon.ico" />
       <title>Mailify | {name}</title>
     </Helmet>
   );

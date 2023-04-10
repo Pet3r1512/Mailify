289743d1daf5cca472e8bedab19524da13ba59fe
Thanh Phong <pttp15122002@gmail.com>
2023-04-10 21:02:30 +0700

    Remove unused

--- ViteJS/vite.config.js
+++ ViteJS/vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { ViteFaviconsPlugin } from "module";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    ViteFaviconsPlugin('./public/images/favicon.ico')
   ],
 })

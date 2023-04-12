d120e856623a9f7df4f937bb16fa89734a7d7008
Thanh Phong <pttp15122002@gmail.com>
2023-04-12 11:42:26 +0700

    Create Inbox page

---
+++ ViteJS/src/pages/Inbox.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Box } from "@mui/material";
+import { Tab } from "../components";
+
+function Inbox() {
+  return (
+    <>
+      <Tab name={"Inbox"} />
+      <Box height={"100vh"}></Box>
+    </>
+  );
+}
+
+export default Inbox;

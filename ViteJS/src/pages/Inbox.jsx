d7c3ca0bed9571b91e65a38f0d5ebc02fe9e278c
Thanh Phong <pttp15122002@gmail.com>
2023-04-20 20:56:45 +0700

    Restricting access to inbox without login token

--- ViteJS/src/pages/Inbox.jsx
+++ ViteJS/src/pages/Inbox.jsx
@@ -2,8 +2,14 @@ import React, { useEffect } from "react";
 
 import { Box } from "@mui/material";
 import { Tab, Template } from "../components";
+import { useNavigate } from "react-router-dom";
 
 function Inbox() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    localStorage.getItem("TOKEN") ? navigate("/inbox") : navigate("/signin");
+  }, []);
+
   return (
     <Box height={"100vh"}>
       <Tab name={"Inbox"} />

b27f55d0eb0ff0c5eaf173fa1823b7063b22286e
Thanh Phong <pttp15122002@gmail.com>
2023-04-20 20:56:14 +0700

    navigate to signin page when regist success

--- ViteJS/src/components/forms/_register.jsx
+++ ViteJS/src/components/forms/_register.jsx
@@ -66,7 +66,7 @@ export default function Register() {
       .then((res) => {
         if (res.success === true) {
           setIsSubmit(false);
-          navigate("/");
+          navigate("/signin");
         } else {
           setIsFailed(true);
           setTimeout(() => {

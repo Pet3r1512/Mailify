0d0f52ce663644b96fb98c9cb9a0dfabf22d9c55
Thanh Phong <pttp15122002@gmail.com>
2023-05-05 22:10:49 +0700

    rename

--- ViteJS/src/routes/index.jsx
+++ ViteJS/src/routes/index.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import Home from "../pages/Home.jsx";
+import Home from "../pages/Home";
 import Setting from "../pages/Setting";
 import RegisterPage from "../pages/RegisterPage";
 import SignInPage from "../pages/SignInPage";

36405a36928a0d9e4d0056e990adcf630b1a6256
Thanh Phong <pttp15122002@gmail.com>
2023-05-05 21:20:08 +0700

    bugfix

--- ViteJS/src/pages/index.jsx
+++ ViteJS/src/pages/index.jsx
@@ -1,6 +1,6 @@
 export { default as SignInPage } from "./Signin";
 export { default as RegisterPage } from "./register";
 export { default as Home } from "./home";
-// export { default as Setting } from "./Setting";
+export { default as Setting } from "./Setting";
 export { default as Loading } from "./Loading";
 export { default as Inbox } from "./Inbox";
